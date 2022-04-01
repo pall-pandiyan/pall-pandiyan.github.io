fe94dfdce8135b0cad4ff45520953bc9f9e23820
Pall Pandiyan <pallpandiyan@gmail.com>
2022-04-01 14:48:59 +0530

    added index.html

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var x = 0;
+function myFunction() {
+    x += 1;
+
+    document.getElementById("result").innerHTML = "You Clicked the button "+ x + " times!";
+
+    /*
+    document.write('<p>Paragraph</p>');
+    alert(x);
+    */
+}
