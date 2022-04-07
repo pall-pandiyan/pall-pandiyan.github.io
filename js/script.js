7bf623c5d5c3f6e24f544300977910e2a302bead
Pall Pandiyan <pallpandiyan@gmail.com>
2022-04-07 15:54:26 +0530

    added a simple js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,5 @@
 var x = 0;
 function myFunction() {
 	x += 1;
-
-	document.getElementById("result").innerHTML =
-		"You Clicked the button " + x + " times!";
-
-	/*
-	document.write('<p>Paragraph</p>');
-	alert(x);
-	*/
+	document.getElementById("result").innerHTML = "You Clicked the button " + x + " times!";
 }
